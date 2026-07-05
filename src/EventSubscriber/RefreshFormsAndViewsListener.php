ae9e95798ad0046c9b3aebab48ace704b43691b6
jannes <jannesdrijkoningen@gmail.com>
2026-07-05 14:41:30 +0200

    add refreshing commands when plugins refresh

--- src/EventSubscriber/RefreshFormsAndViewsListener.php
+++ src/EventSubscriber/RefreshFormsAndViewsListener.php
@@ -5,16 +5,23 @@ declare(strict_types=1);
 namespace Forumify\Discord\EventSubscriber;
 
 use Forumify\Discord\ForumifyDiscordPlugin;
+use Forumify\Discord\Service\BotService;
 use Forumify\Plugin\Event\PluginRefreshedEvent;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 
 #[AsEventListener(PluginRefreshedEvent::class)]
 class RefreshFormsAndViewsListener
 {
+    public function __construct(private readonly BotService $botService)
+    {
+    }
+
     public function __invoke(PluginRefreshedEvent $event): void
     {
         if ($event->plugin->getPluginClass() !== ForumifyDiscordPlugin::class) {
             return;
         }
+
+        $this->botService->fetchData('refreshSlashCommands');
     }
 }
